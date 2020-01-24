60b31a019348be80b1f2188be9f39aa876e71498
BChlebda <bartlomiej.chlebda@gmail.com>
2020-01-24 15:31:02 +0100

    next update

--- script/lightbox.js
+++ script/lightbox.js
@@ -1,36 +1,50 @@
 $(document).ready(function(){
-    function stopScrolling(){   
-            $(window).on("mousewheel DOMMouseScroll",function(){
-            return false;
-            });
-            }
-    function startScrolling(){   
-            $(window).unbind("mousewheel DOMMouseScroll");
-            }
-    
-    
-    
-    $('.lt_switch').click(function(){
-        $('.lt_backdrop').animate({'opacity':'.50'}, 300, 'linear');
-        $('.lt_box').animate({'opacity':'1.00'}, 300, 'linear');
-        $('.lt_backdrop, .lt_box').css('display', 'block');
-        stopScrolling();
-    });
-    
-    var width = 820;
+
+    var width = 1100;
     var animationSpeed = 1000;
-    var pause = 6000;
-    var currentSlide = 1;
+    var pause = 5000;
     var interval;
     
+    var currentSlide = 1;    
     var $Slider = $('#lt_Slider');
     var $Slides = $Slider.find('.lt_slides');
     var $Slide = $Slides.find('.lt_slide');
+    var real = $Slide.length;
+    alert(real);
+    
+    function stopScrolling(){   
+            $('.light').on('wheel', function(event2){
+            event2.preventDefault();
+            });
+            }
+    function startScrolling(){   
+            $('.light').unbind('wheel');
+            }
+    
+    $('.lt_switch').click(function(){
+        var pos = $('#projects').position();
+        console.log(pos.top, pos.left);
+        $('.light').css('top', pos.top);
+        $('.light').css('z-index', '49');
+        $('.lt_backdrop').animate({'opacity':'.50'}, 500, 'linear');
+        $('.lt_box').animate({'opacity':'1.00'}, 300, 'linear');
+        $('.lt_backdrop, .lt_box').css('display', 'block');
+        stopScrolling();
+        if(real > 1){        
+        addSlide();
+        startSlider();
+        $Slider.on('mouseover', stopSlider).on('mouseleave', startSlider);    
+        }
+    });
+    
+    function addSlide(){
+        var ost = '<li class="lt_slide">'+$($Slide[0]).html();+'</li>';
+        $('.lt_slides').append(ost);
+        $Slide = $Slides.find('.lt_slide');
+        $('.lt_slides').css('width', width*$Slide.length);
+    }
 
     function startSlider(){
-        if($Slide.length == 1) {
-            stop();
-        } else {
         interval = setInterval(function(){
         $Slides.animate({'margin-left': '-='+width}, animationSpeed, function(){
             currentSlide++;
@@ -39,49 +53,41 @@ $(document).ready(function(){
                 currentSlide = 1;
             }
         });
-        }, pause);    
-        }
+    }, pause);    
     }
     
     function stopSlider(){
         clearInterval(interval);
-    }
-    
-    $Slider.on('mouseover', stopSlider).on('mouseleave', startSlider);
-    
-    startSlider();
+    }    
     
     $('.lt_prev').click(function(){
         stopSlider();
             if(currentSlide == 1 ){
                 stop();
             } else {       
-        $Slides.animate({'margin-left': '+='+width}, animationSpeed, function(){ 
-            currentSlide--;
-        });        
+                $Slides.animate({'margin-left': '+='+width}, animationSpeed, function(){ 
+                currentSlide--;
+                });        
             }
     });
     
     $('.lt_next').click(function(){
         stopSlider();
-            if(currentSlide == $Slide.length ){
+            if(currentSlide == real ){
                 stop();
             } else {        
-        $Slides.animate({'margin-left': '-='+width}, animationSpeed, function(){
-            currentSlide++;
-            if(currentSlide == $Slide.length){
-                $Slides.css('margin-left', 0);
-                currentSlide = 1;
-            }
-        });
+                $Slides.animate({'margin-left': '-='+width}, animationSpeed, function(){
+                currentSlide++;
+                });
         }
     });
     
-    
     $('.lt_close').click(function(){
         startScrolling();
+        stopSlider();
         $('.lt_backdrop, .lt_box').animate({'opacity':'0'}, 400, 'linear', function(){
         $('.lt_backdrop, .lt_box').css('display', 'none');
+        $('.light').css('z-index', '-1');
         });
     });
 });
